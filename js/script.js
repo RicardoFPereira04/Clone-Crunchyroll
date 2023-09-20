90aa0b221acdd6b0c6fa5638513fbf02b6645bc3
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2023-09-20 18:09:32 -0300

    continuing-header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
     // Quando o botão de dropdown for clicado
-    $(".dropdown-toggle").click(function() {
+    $("#drop").click(function() {
       // Mostra o dropdown personalizado
       $(".custom-dropdown").toggle();
+      
+      // Aplica o efeito de desfoque no resto da tela
+      $(".overlay").toggle();
+
+      
+    });
+  });
+
+
+
+
+  $(document).ready(function() {
+    // Quando o botão de dropdown for clicado
+    $("#drop-2").click(function() {
+      // Mostra o dropdown personalizado
+      $("#custom-drop").toggle();
       
       // Aplica o efeito de desfoque no resto da tela
       $(".overlay").toggle();
