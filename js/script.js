74087f24e96b30b42fe2513a18707d98d494b347
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2023-09-22 18:20:59 -0300

    add-animations-dropdown

--- js/script.js
+++ js/script.js
@@ -1,26 +1,52 @@
 $(document).ready(function() {
-    // Quando o botão de dropdown for clicado
-    $("#drop").click(function() {
-      // Mostra o dropdown personalizado
-      $(".custom-dropdown").toggle();
-      
-      // Aplica o efeito de desfoque no resto da tela
-      $(".overlay").toggle();
+  // Função para abrir e fechar o dropdown de "Navegar"
+  function toggleDropdownNav() {
+    $(".custom-dropdown").toggleClass("show");
+    $(".overlay").toggle();
+    if ($(".custom-dropdown").hasClass("show")) {
+      $("#drop").addClass("active-hover");
+      $("#drop-2").removeClass("active-hover");
+    } else {
+      $("#drop").removeClass("active-hover");
+    }
 
-      
-    });
+    if ($(".custom-dropdown").is(":visible")) {
+      $("#drop").addClass("active-hover");
+    } else {
+      $("#drop-2").removeClass("active-hover");
+    }
+  }
+
+  // Função para abrir e fechar o dropdown de "Notícias"
+  function toggleDropdownNews() {
+    $(".custom-dropdown2").toggleClass("show");
+    $(".overlay").toggle();
+    if ($(".custom-dropdown2").hasClass("show")) {
+      $("#drop-2").addClass("active-hover");
+      $("#drop").removeClass("active-hover");
+    } else {
+      $("#drop-2").removeClass("active-hover");
+    }
+  }
+
+  // Quando o botão de dropdown para "Navegar" for clicado
+  $("#drop").click(function(e) {
+    e.stopPropagation(); // Evita que o evento de clique se propague para o documento
+    toggleDropdownNav();
   });
 
-
-
-
-  $(document).ready(function() {
-    // Quando o botão de dropdown for clicado
-    $("#drop-2").click(function() {
-      // Mostra o dropdown personalizado
-      $("#custom-drop").toggle();
-      
-      // Aplica o efeito de desfoque no resto da tela
-      $(".overlay").toggle();
-    });
+  // Quando o botão de dropdown para "Notícias" for clicado
+  $("#drop-2").click(function(e) {
+    e.stopPropagation(); // Evita que o evento de clique se propague para o documento
+    toggleDropdownNews();
   });
+
+  // Fecha o menu dropdown quando clicar em qualquer parte da tela fora do menu
+  $(document).on("click", function(event) {
+    if (!$(event.target).closest(".dropdown").length) {
+      $(".custom-dropdown, .custom-dropdown2").removeClass("show");
+      $(".overlay").hide();
+      $("#drop, #drop-2").removeClass("active-hover");
+    }
+  });
+});
